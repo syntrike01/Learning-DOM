399237f2e0326b908f40d47eb76908d62f630589
Akash Gogoi <gogoiakash09@gmail.com>
2025-08-18 21:10:00 +0530

    add items on the list using form

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
-
-
-const formEl = document.getElementById('addForm');
-const inputEl = document.getElementById('item');
-const ulEl = document.getElementById('items');
+const formEl = document.getElementById("addForm");
+const ulEl = document.getElementById("items");
 const submitBtn = formEl.querySelector("input[type='submit']");
 
-submitBtn.addEventListener('click',runEvent);
+submitBtn.addEventListener("click", runEvent);
 
-function runEvent(e){
-    e.preventDefault();
-    console.log(inputEl.value)
-    console.log(newLi)
+function runEvent(e) {
+  e.preventDefault();
+  const inputEl = document.getElementById("item");
+  console.log(inputEl.value);
+
+  // create element
+  let newLi = document.createElement('li');
+
+  newLi.className = 'list-group-item';
+
+  let newDivText = document.createTextNode(inputEl.value);
+  newLi.appendChild(newDivText);
+
+  ulEl.appendChild(newLi);
 }
-
-
-
-// create element
-let newLi = document.createElement('li');
-
-newLi.className = 'ist-group-item d-flex justify-content-between align-items-center';
-
-let newDivText = document.createTextNode(inputEl.value);
-newLi.appendChild(newDivText)
-
